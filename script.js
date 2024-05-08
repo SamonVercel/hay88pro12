1948911f783fdedfd3cf5baf15648ede86131f2e
samonvercel <samonvercel7777@gmail.com>
2024-05-08 10:26:19 +0700

    first commit

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-// import brightBtn from "./Image/brightBtn";
-
 const btnImage = document.querySelectorAll(".btn");
 var hideButton = document.querySelector(".hide-button");
 var navId = document.querySelector("#nav-id");
 var showButton = document.querySelector(".show-button");
-
-// for (let i = 0; i < btnImage.length; i++) {
-//   btnImage[i].addEventListener("mouseover", function () {
-//     this.style.backgroundImage = "url(image/brightBtn.png)";
-//   });
-
-//   btnImage[i].addEventListener("mouseout", function () {
-//     this.style.backgroundImage = "url(image/lowBtn.png)";
-//   });
-// }
 
 hideButton.addEventListener("click", () => {
   hideButton.style = "right: 50%";
@@ -29,11 +17,9 @@ showButton.addEventListener("click", () => {
   hideButton.style = "right: 20px";
 });
 
-// Get the button:
 let topButton = document.querySelector(".top-button");
 topButton.addEventListener("click", topFunction);
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
@@ -46,8 +32,7 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
